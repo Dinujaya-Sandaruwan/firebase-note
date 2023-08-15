0a6a1898df79915d35c25c287eb9be218d681be6
Dinujaya Sandaruwan <info.dinujaya@gmail.com>
2023-08-15 05:30:00 +0530

    Sign in using email and password

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import Auth from "./components/Auth";
+
 function App() {
   return (
     <>
       <h1>Firebase Note</h1>
+
+      <Auth />
     </>
   );
 }
---
+++ src/components/Auth.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { auth } from "../config/firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+
+const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const signIn = async () => {
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  console.log(auth.currentUser?.email);
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Email..."
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password..."
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={signIn}>Sign in</button>
+    </div>
+  );
+};
+
+export default Auth;
